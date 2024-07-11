6d56b76bac0e01db83284759d0db4c9bad4ca7a7
Sebastian Heusinger <heusings@googlemail.com>
2024-07-11 13:24:16 +0200

    added test for receiving messages

--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 
 pub const dbus = @import("dbussignal");
 
+const DBUS_PATH = "/zig/timekeeper/signal";
+const DBUS_INTERFACE = "zig.timekeeper.signal";
+const DBUS_MEMBER = "msg";
+
 test {
     std.testing.refAllDecls(@This());
 }
@@ -13,4 +17,25 @@ test "test send to dbus" {
     const data_len = (try std.fmt.bufPrint(&buf, "{s}", .{"test"})).len;
     buf[data_len] = 0;
     try bus.send_message("test", &buf);
+}
+
+test "receive message" {
+    var gp = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
+    const allocator = gp.allocator();
+
+    defer _ = gp.deinit();
+
+    var bus = try dbus.DBus.init(DBUS_PATH, DBUS_INTERFACE);
+    var count: u32 = 0;
+    while (true) {
+        const value = try bus.get_message_blocking(200, DBUS_MEMBER, allocator);
+        count += 1;
+        if (value) |val| {
+            allocator.free(val);
+        }
+
+        if (count > 5) {
+            break;
+        }
+    }
 }
